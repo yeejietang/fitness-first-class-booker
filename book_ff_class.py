67ad611ad941714ec7adad08cc0d6879b5f5c3ef
Yee Jie Tang <yeejietang@gmail.com>
2019-09-23 20:44:02 +0800

    async booking

--- book_ff_class.py
+++ book_ff_class.py
@@ -9,21 +9,20 @@ Created on Tue Jul  9 15:12:50 2019
 import time
 import pause
 from selenium.webdriver import Chrome
-from credentials import ff_web_login
+from credentials import ff_web_login, ff_valerie_login
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 
 class ffchrome(Chrome):
 
     executable_path = '/usr/local/bin/chromedriver' 
     # Tells driver to implicitly wait for elements for 5 seconds before timing out
-    time_to_wait = 5 
+    time_to_wait = 7 
     ff_website = 'https://clients.mindbodyonline.com/LoginLaunch?studioid=741470'
     gym_selector = 'a.selectBox.bf-filter.selectBox-dropdown' 
-    login = ff_web_login['login'] 
-    password = ff_web_login['password']
     drivers = [] # registrar 
     
-    def __init__(self, gym):
+    def __init__(self, gym, login, password):
         self.drivers.append(self) 
         Chrome.__init__(self, self.executable_path) 
         Chrome.implicitly_wait(self, self.time_to_wait) 
@@ -33,8 +32,8 @@ class ffchrome(Chrome):
         # Choose gym
         Chrome.find_element_by_link_text(self, gym).click() 
         # Login
-        Chrome.find_element_by_id(self, 'requiredtxtUserName').send_keys(self.login) 
-        Chrome.find_element_by_id(self, 'requiredtxtPassword').send_keys(self.password) 
+        Chrome.find_element_by_id(self, 'requiredtxtUserName').send_keys(login) 
+        Chrome.find_element_by_id(self, 'requiredtxtPassword').send_keys(password) 
         Chrome.find_element_by_id(self, 'btnLogin').click() 
         
     def book_class(self, class_button):
@@ -55,6 +54,13 @@ class ffchrome(Chrome):
         for driver in cls.drivers:
             driver.quit()
 
+def refresh_and_book_class(driver, class_button):
+    driver.refresh()
+    driver.find_element_by_name(class_button).click()
+    driver.find_element_by_xpath("//input[@value='Make a single reservation']").click() 
+    time.sleep(2)
+    driver.quit()
+
 # Check which day are we running this script 
 # dow = Monday is 0, Sunday is 6
 dow = datetime.today().weekday()
@@ -71,10 +77,21 @@ dow = datetime.today().weekday()
 # BODYPUMP, Sunday 4:55pm, Paragon (Bertram)                    but7223
 # Circuit, Monday 6:40pm, Paragon (Yeoh)                        but7284
 
+# Get credentials
+yj_login = ff_web_login['login']
+yj_pwd = ff_web_login['password']
+val_login = ff_valerie_login['login']
+val_pwd = ff_valerie_login['password']
+
 # Initialize browser sessions
-if (dow == 0 or dow == 1):
-    driver1 = ffchrome('MBFC')
-    driver2 = ffchrome('MBFC')
+if dow == 0:
+    driver1 = ffchrome('MBFC', yj_login, yj_pwd)
+    driver2 = ffchrome('MBFC', yj_login, yj_pwd)
+elif dow == 1:
+    driver1 = ffchrome('MBFC', yj_login, yj_pwd)
+    driver2 = ffchrome('MBFC', yj_login, yj_pwd)
+    driver3 = ffchrome('MBFC', val_login, val_pwd)
+    driver4 = ffchrome('MBFC', val_login, val_pwd)
 # elif dow == 2:
 #     driver1 = ffchrome('MarketStreet')
 # elif dow == 3:
@@ -94,30 +111,30 @@ dt = datetime.now()
 y, m, d = dt.year, dt.month, dt.day
 pause.until(datetime(y, m, d, 8))
 
+# Start timer
+# start = time.time()
+
 # Refresh
-ffchrome.refresh_all()
+# ffchrome.refresh_all()
 
 # Book gym classes
-if dow == 0:
-    driver1.book_class('but820')
-    driver2.book_class('but2700')
-elif dow == 1:
-    driver1.book_class('but902')
-    driver2.book_class('but906')
-# elif dow == 2:
-#     driver1.book_class('but6370')
-# elif dow == 3:
-#     driver1.book_class('but7197')
-#     driver2.book_class('but2705')
-#     driver3.book_class('but1467')
-# elif dow == 4:
-#     driver1.book_class('but7223')
-# elif dow == 5:
-#     driver1.book_class('but7284')
+with ThreadPoolExecutor(max_workers=None) as executor:
+    if dow == 0:
+        futures = [ executor.submit(refresh_and_book_class, driver1, 'but820'), 
+                    executor.submit(refresh_and_book_class, driver2, 'but2700') ] 
+    elif dow == 1:
+        futures = [ executor.submit(refresh_and_book_class, driver1, 'but902'), 
+                    executor.submit(refresh_and_book_class, driver2, 'but906'),
+                    executor.submit(refresh_and_book_class, driver1, 'but902'), 
+                    executor.submit(refresh_and_book_class, driver2, 'but906') ] 
 
 # Click all book buttons
-ffchrome.click_book_button()
+# ffchrome.click_book_button()
+
+# End timer
+# end = time.time()
+# print("Time Taken: {:.6f}s".format(end-start))
 
 # Wait 2 seconds before quitting drivers
-time.sleep(2)
-ffchrome.quit_all()
+# time.sleep(2)
+# ffchrome.quit_all()
